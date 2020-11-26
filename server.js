3cf2b00b429f3e47f268d785467f0844a8026bfb
Marshysaurus <ryuzaki.orozco@gmail.com>
2020-11-26 15:24:46 -0600

    Activity

--- server.js
+++ server.js
@@ -1,46 +1,49 @@
 // Imports
-const express = require('express');
-const webRoutes = require('./routes/web');
+const express = require("express");
+const webRoutes = require("./routes/web");
 
 // Session imports
-let cookieParser = require('cookie-parser');
-let session = require('express-session');
-let flash = require('express-flash');
-let passport = require('passport');
+let cookieParser = require("cookie-parser");
+let session = require("express-session");
+let flash = require("express-flash");
+let passport = require("passport");
 
 // Express app creation
 const app = express();
-
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
 
 // Configurations
-const appConfig = require('./configs/app');
+const appConfig = require("./configs/app");
 
 // View engine configs
-const exphbs = require('express-handlebars');
+const exphbs = require("express-handlebars");
 const hbshelpers = require("handlebars-helpers");
 const multihelpers = hbshelpers();
-const extNameHbs = 'hbs';
+const extNameHbs = "hbs";
 const hbs = exphbs.create({
   extname: extNameHbs,
-  helpers: multihelpers
+  helpers: multihelpers,
 });
 app.engine(extNameHbs, hbs.engine);
-app.set('view engine', extNameHbs);
+app.set("view engine", extNameHbs);
 
 // Session configurations
-let sessionStore = new session.MemoryStore;
+let sessionStore = new session.MemoryStore();
 app.use(cookieParser());
-app.use(session({
-  cookie: { maxAge: 60000 },
-  store: sessionStore,
-  saveUninitialized: true,
-  resave: 'true',
-  secret: appConfig.secret
-}));
+app.use(
+  session({
+    cookie: { maxAge: 60000 },
+    store: sessionStore,
+    saveUninitialized: true,
+    resave: "true",
+    secret: appConfig.secret,
+  })
+);
 app.use(flash());
 
 // Passport configurations
-require('./configs/passport');
+require("./configs/passport");
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -48,12 +51,29 @@ app.use(passport.session());
 app.use(express.urlencoded({ extended: true }));
 
 // Route for static files
-app.use('/', express.static(__dirname + '/public'));
+app.use("/", express.static(__dirname + "/public"));
 
 // Routes
-app.use('/', webRoutes);
+app.use("/", webRoutes);
+
+io.on("connection", (socket) => {
+  // Recibe la conexión del cliente
+  console.log("Client connected...");
+  let i = 0;
+  // Emite un mensaje
+  setInterval(() => {
+    socket.emit("toast", { message: `Message: ${i}` });
+    i++;
+  }, 3000);
+  // Recibe un mensaje
+  socket.on("messageToServer", (data) => {
+    console.log("messageReceivedFromClient: ", data.text);
+  });
+});
 
 // App init
-app.listen(appConfig.expressPort, () => {
-  console.log(`Server is listenning on ${appConfig.expressPort}! (http://localhost:${appConfig.expressPort})`);
+server.listen(appConfig.expressPort, () => {
+  console.log(
+    `Server is listenning on ${appConfig.expressPort}! (http://localhost:${appConfig.expressPort})`
+  );
 });
